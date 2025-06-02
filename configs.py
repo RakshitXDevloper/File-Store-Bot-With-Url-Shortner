9af8ea879ed6dd610d7c985d5c50efcaf69fd7b7
RakshitXDevloper <hacktivemindhub@gmail.com>
2025-06-02 14:20:52 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "https://atglinks.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "9afd00d1b04756a387d5db35fa4b0ece9d629324")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6287430419"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Rakshit:<db_password>@cluster0.g0qid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "2624805754")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "2567063861"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
